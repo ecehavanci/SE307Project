ab4c0186623a469d8fa03b74ecf246b2e23c8e79
ecehavanci <ecehavanci@gmail.com>
2022-12-14 10:20:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SE307Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
