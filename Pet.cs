8e1c8e78ae671a70a462f3a105c18db622febb96
silaserter <silasserter@gmail.com>
2022-12-17 01:15:38 +0300

    Pet related activities added to Pet & Pet Owner

--- Pet.cs
+++ Pet.cs
@@ -1,30 +1,157 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SE307Project
 {
     public class Pet
     {
-        private int Age;
-        private string Breed;
-        private string Name;
-        private List<CareType> CareRoutine;
-        private AnimalType Species;
+        private String Name { set; get; }
+
+        public String _Name
+        {
+            get { return Name; }
+            set { Name = _Name; }
+        }
+
+        private int Age { set; get; }
+
+        public int _Age
+        {
+            get { return Age; }
+            set { Age = _Age; }
+        }
+
+        private String Breed { set; get; }
+
+        public String _Breed
+        {
+            get { return Breed; }
+            set { Breed = _Breed; }
+        }
+
+        private AnimalType Species { set; get; }
+
+        public String _Species
+        {
+            get { return Species.ToString().Replace("Species.", "").Replace("A", " A"); }
+            set
+            {
+                if (_Species == "Dog")
+                {
+                    Species = AnimalType.Dog;
+                }
+                else if (_Species == "Cat")
+                {
+                    Species = AnimalType.Cat;
+                }
+                else if (_Species == "Bird")
+                {
+                    Species = AnimalType.Bird;
+                }
+                else if (_Species == "Hamster")
+                {
+                    Species = AnimalType.Hamster;
+                }
+                else if (_Species == "Exotic Animal")
+                {
+                    Species = AnimalType.ExoticAnimal;
+                }
+            }
+        }
+
+        private List<CareType> CareRoutine { get; }
 
         public Pet()
         {
             CareRoutine = new List<CareType>();
         }
+
+        public Pet(String name, int age, String breed, AnimalType species)
+        {
+            Name = name;
+            Age = age;
+            Breed = breed;
+            Species = species;
+            CareRoutine = new List<CareType>();
+        }
+
+
+        public void AddCareRoutine(String careType)
+        {
+            careType = careType.ToUpper();
+            if (careType == "BATHE")
+            {
+                CareRoutine.Add(CareType.Bathe);
+            }
+            else if (careType == "WALK")
+            {
+                CareRoutine.Add(CareType.Walk);
+            }
+            else if (careType == "COMB")
+            {
+                CareRoutine.Add(CareType.Comb);
+            }
+            else if (careType == "FEED")
+            {
+                CareRoutine.Add(CareType.Feed);
+            }
+            else if (careType == "TAKE TO VET")
+            {
+                CareRoutine.Add(CareType.TakeToVet);
+            }
+            else if (careType == "PLAY")
+            {
+                CareRoutine.Add(CareType.Play);
+            }
+            else if (careType == "GIVE MEDS")
+            {
+                CareRoutine.Add(CareType.GiveMeds);
+            }
+        }
+
+        public void AddCareRoutine(int index)
+        {
+            if (index == 1)
+            {
+                CareRoutine.Add(CareType.Bathe);
+            }
+            else if (index == 2)
+            {
+                CareRoutine.Add(CareType.Walk);
+            }
+            else if (index == 3)
+            {
+                CareRoutine.Add(CareType.Comb);
+            }
+            else if (index == 4)
+            {
+                CareRoutine.Add(CareType.Feed);
+            }
+            else if (index == 5)
+            {
+                CareRoutine.Add(CareType.TakeToVet);
+            }
+            else if (index == 6)
+            {
+                CareRoutine.Add(CareType.Play);
+            }
+            else if (index == 7)
+            {
+                CareRoutine.Add(CareType.GiveMeds);
+            }
+        }
     }
 
-    enum AnimalType    {
+    public enum AnimalType
+    {
         Cat,
         Dog,
+        Bird,
         Hamster,
-        ExoticAnimal,
-        Bird
+        ExoticAnimal
     }
 
-    enum CareType
+    public enum CareType
     {
         Bathe,
         Walk,
--- PetOwner.cs
+++ PetOwner.cs
@@ -9,45 +9,120 @@ namespace SE307Project
         private List<PetSitter> HiredPetSitters;
         private List<Pet> Pets;
 
-        public PetOwner(string name, string surname, string email, string password, string Location) : base(name, surname, email, password, Location)
+        public PetOwner(string name, string surname, string email, string password, string Location) : base(name,
+            surname, email, password, Location)
         {
             HiredPetSitters = new List<PetSitter>();
             Pets = new List<Pet>();
         }
 
-        public void AddPet(Pet pet) //in UML it was String,int,String,String
+        public void AddPet(String name, int age, String breed, AnimalType species)
         {
-            
+            Pets.Add(new Pet(name, age, breed, species));
+        }
+
+        //If given pet name is in the list it returns the pet othervise it returns null so a null reference exception
+        //to be thrown and be handled
+        public Pet FindPetInPets(String PetName)
+        {
+            foreach (var pet in Pets)
+            {
+                if (PetName == pet._Name)
+                {
+                    return pet;
+                }
+            }
+
+            return null;
         }
 
         public void DeletePet(String PetName)
         {
-
+            try
+            {
+                Pets.Remove(FindPetInPets(PetName));
+            }
+            catch (NullReferenceException n)
+            {
+                Console.WriteLine("There is no pet with the given name");
+            }
         }
 
-        public int EditPet()
+        public void DeletePet(int index)
         {
+            Pets.RemoveAt(index);
+        }
+
+        public int EditPet(String PetName)
+        {
+            Pet pet;
+            try
+            {
+                pet = FindPetInPets(PetName);
+            }
+            catch (NullReferenceException n)
+            {
+                Console.WriteLine("There is no pet with the given name");
+                return -1;
+            }
+
+            Console.WriteLine("Enter new properties, enter -1 to skip");
+
+            Console.Write("Name: ");
+            String name = Console.ReadLine();
+
+            Console.Write("Age: ");
+            String age = Console.ReadLine();
+
+            ListPetSpecies();
+            Console.Write("Species: ");
+            String species = Console.ReadLine();
+
+            Console.Write("Breed: ");
+            String breed = Console.ReadLine();
+
+            if (name != "-1")
+            {
+                pet._Name = name;
+            }
+            
+            if (age != "-1")
+            {
+                pet._Age = int.Parse(age);
+            }
+            
+            if (species != "-1")
+            {
+                pet._Species = species;
+            }
+            
+            if (breed != "-1")
+            {
+                pet._Breed = breed;
+            }
+
             return 0;
+        }
+
+        public void ListPetSpecies()
+        {
+            Console.WriteLine("Possible species");
+            Console.WriteLine("1) Dog");
+            Console.WriteLine("2) Cat");
+            Console.WriteLine("3) Bird");
+            Console.WriteLine("4) Hamster");
+            Console.WriteLine("5) Exotic Animal");
         }
 
         public void EditProfile()
         {
-
         }
-
-        private Pet FindPetInPets(String PetName)
-        {
-            return new Pet();
-        }
-
         public void HirePetSitter(PetSitter petSitter)
         {
-
         }
 
         public void MakeCommentToPetSitter()
         {
-
         }
 
         public int RatePetSitter(PetSitter petSitter)
@@ -57,19 +132,14 @@ namespace SE307Project
 
         public void SendMessageToPetSitter(PetSitter petSitter)
         {
-
         }
+
         public void SendRequestToPetSitter(PetSitter petSitter)
         {
-
         }
 
-        public void ShowMessagesFor(String UserName) 
+        public void ShowMessagesFor(String UserName)
         {
-
         }
-
-
-
     }
 }
