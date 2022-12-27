7d267c95d74eb2f93b5b962463b34a70ad82aaee
ecehavanci <ecehavanci@gmail.com>
2022-12-27 19:12:43 +0300

    Exceptions moved to exception class

--- Exceptions.cs
+++ Exceptions.cs
@@ -19,4 +19,47 @@ namespace SE307Project
 
     }
 
+    class ExceptionWrongPassword : Exception
+    {
+        private String Password;
+
+        public ExceptionWrongPassword(string password) : base("Entered password is wrong! Try Again.")
+        {
+            Password = password;
+        }
+
+        public void PrintException()
+        {
+            Console.WriteLine(Message);
+        }
+    }
+    class ExceptionWrongEmail : Exception
+    {
+        private String Email;
+
+        public ExceptionWrongEmail(string email) : base("Entered email does not exist! Try Again.")
+        {
+            Email = email;
+        }
+
+        public void PrintException()
+        {
+            Console.WriteLine(Message);
+        }
+    }
+    class ExceptionAlreadyExistEmail : Exception
+    {
+        private String Email;
+
+        public ExceptionAlreadyExistEmail(string email) : base("Entered email is already exist. Please use new one.")
+        {
+            Email = email;
+        }
+
+        public void PrintException()
+        {
+            Console.WriteLine(Message);
+        }
+    }
+
 }
